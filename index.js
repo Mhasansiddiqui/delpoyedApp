92a9037c8ffc9bb4fdb46d00bef7bea595a932e4
hasang hasan828 <hasang hasan828>
2016-05-26 15:51:33 -0700

    ccc

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ var app = express();
 
 app.set("port", (process.env.PORT  || 5000));
 
-app.use(function(req,res ,next){
-   res.send('hi hasan');
-    next();
-})
+app.use(express.static('public'));
+
 
 
 app.listen(app.get("port"), function () {
