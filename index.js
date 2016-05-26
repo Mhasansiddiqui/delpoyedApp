feeb99f0badabdcc8765f04e29e94f10bca794bb
hasang hasan828 <hasang hasan828>
2016-05-26 14:22:55 -0700

    a

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = express();
 
 
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.set("port", (process.env.PORT  || 5000));
 
 app.use(function(req,res ,next){
---
+++ public/module.js
@@ -0,0 +1,21 @@
+angular.module('App' , ['ngMaterial' , 'ui.router' ,'duScroll'  ] )
+   .controller('MyCtrl', function($scope, $document){
+
+
+
+    $document.scrollTopAnimated(0, 2000).then(function() {
+      
+      });
+
+
+    $scope.toTheTop = function() {
+      $document.scrollTopAnimated(0, 5000).then(function() {
+       
+      });
+    }
+    var section3 = angular.element(document.getElementById('section-3'));
+    $scope.toSection3 = function() {
+      $document.scrollToElementAnimated(section3);
+    }
+  }
+).value('duScrollOffset', 30);
