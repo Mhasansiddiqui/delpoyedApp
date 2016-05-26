e518a797f9c336f0baf554c89c41488aa4242e5d
hasang hasan828 <hasang hasan828>
2016-05-26 14:31:35 -0700

    mm

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 app.set("port", (process.env.PORT  || 5000));
 
 app.use(function(req,res ,next){
-   res.sendFile(path.join(__dirname, './public', 'index.html'));
+   res.sendFile('public/index.html' , { root : __dirname});
    next();
 })
 
