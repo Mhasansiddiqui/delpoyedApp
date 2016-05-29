559582f4f23e8c98d18508f29cd33646e7f786b2
Mhasansiddiqui <hasancomsoft@gmail.com>
2016-05-29 02:50:27 -0700

    a

--- public/firebasejs.js
+++ public/firebasejs.js
@@ -1,8 +1,24 @@
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCqNkunB6NDmV_Rd47JFzndg_YzwWKXjeE",
-    authDomain: "project-7305199249666167418.firebaseapp.com",
-    databaseURL: "https://project-7305199249666167418.firebaseio.com",
-    storageBucket: "project-7305199249666167418.appspot.com",
-  };
-  firebase.initializeApp(config);
+ 
+angular.module('App'  )
+   .controller('FormCtrl', function($scope, $document){
+
+		 // Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyCqNkunB6NDmV_Rd47JFzndg_YzwWKXjeE",
+		    authDomain: "project-7305199249666167418.firebaseapp.com",
+		    databaseURL: "https://project-7305199249666167418.firebaseio.com",
+		    storageBucket: "project-7305199249666167418.appspot.com",
+		  };
+		  firebase.initializeApp(config);
+
+
+		  // Get a reference to the database service
+		  var database = firebase.database();
+		$scope.save = function(){
+		 firebase.database().ref('anonymouse/').set($scope.user);
+
+		 firebase.database().ref('anonymouse/').on('value', function(snapshot) {
+		      console.log(snapshot);
+		});
+	}
+});
