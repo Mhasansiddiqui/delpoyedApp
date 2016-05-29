9c1bb2e4e9941e3482968921606b03623fb9a425
Mhasansiddiqui <hasancomsoft@gmail.com>
2016-05-29 00:05:46 -0700

    a

---
+++ public/firebasejs.js
@@ -0,0 +1,8 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCqNkunB6NDmV_Rd47JFzndg_YzwWKXjeE",
+    authDomain: "project-7305199249666167418.firebaseapp.com",
+    databaseURL: "https://project-7305199249666167418.firebaseio.com",
+    storageBucket: "project-7305199249666167418.appspot.com",
+  };
+  firebase.initializeApp(config);
